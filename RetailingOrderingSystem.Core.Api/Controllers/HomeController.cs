e34f6fdac463667c3172dc39e47eea29c1aa646f
YaqubAliy02 <yaqubaliy02@gmail.com>
2025-01-14 11:43:19 +0500

    INFRA: Init Project

---
+++ RetailingOrderingSystem.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailingOrderingSystem.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to Retailing Ordering System");
+        }
+    }
+}
+
