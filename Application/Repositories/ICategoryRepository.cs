adef028674e52aa0fbfa5473766a2e3824c2b58d
YaqubAliy02 <yaqubaliy02@gmail.com>
2025-01-16 11:17:30 +0500

    SERVICES: Implement Category Services

---
+++ Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Application.Repositories;
+using Domain.Models;
+
+namespace Application.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
---
+++ Infrastructure/Services/CategoryRepository.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq.Expressions;
+using Application.Abstraction;
+using Application.Repository;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IRetailingOrderingSystemDbContext context;
+
+        public CategoryRepository(IRetailingOrderingSystemDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            await this.context.Categories.AddAsync(category);
+            int result = await this.context.SaveChangesAsync();
+
+            if (result > 0) return category;
+
+            return null;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var category = await this.context.Categories.FindAsync(id);
+
+            if (category is not null)
+            {
+                this.context.Categories.Remove(category);
+            }
+
+            int result = await this.context.SaveChangesAsync();
+
+            if (result > 0) return true;
+
+            return false;
+        }
+
+        public async Task<IQueryable<Category>> GetAllAsync(Expression<Func<Category, bool>> expression)
+        {
+            return this.context.Categories.Where(expression)
+                .Include(category => category.Products);
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return this.context.Categories
+                .Where(c => c.Id.Equals(id))
+                .Include(category => category.Products)
+                .FirstOrDefault();
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            var existingCategory = await GetByIdAsync(category.Id);
+
+            if (existingCategory is not null)
+            {
+                existingCategory.Name = category.Name;
+
+                foreach (var product in existingCategory.Products)
+                {
+                    var existingProducts = this.context.Products.FindAsync(product.Id);
+
+                    if (existingCategory is not null)
+                    {
+                        existingCategory.Products.Add(product);
+                    }
+                }
+
+                var result = await this.context.SaveChangesAsync();
+
+                if (result > 0) return category;
+            }
+
+            return null;
+        }
+    }
+}
